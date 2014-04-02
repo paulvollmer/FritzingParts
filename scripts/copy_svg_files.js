677e925a3871d1bb880435f3ab31dec8d2774961
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2014-04-02 11:18:11 +0200

    WIP at copy sag's script

--- scripts/copy_svg_files.js
+++ scripts/copy_svg_files.js
@@ -1,3 +1,38 @@
 // scripts/copy_svg_files.js
 
-console.log('copy svg');
+var fs = require('fs');
+var parts = require('../parts.json');
+
+for (var i=0; i<parts.length; i++) {
+  //console.log(parts[i]);
+  var fzpJson = require('../parts/'+parts[i]+'/index.json');
+
+  var breadboardFilepath = fzpJson.views.breadboard.image;
+  try  {
+    fs.writeFileSync('./parts/'+parts[i]+'/breadboard/'+parts[i]+'.svg', fs.readFileSync('./fritzing/fritzing/parts/svg/core/'+breadboardFilepath))
+  } catch(e) {
+    console.log('breadboard: '+e);
+  }
+
+  var pcbFilepath = fzpJson.views.pcb.image;
+  try  {
+    fs.writeFileSync('./parts/'+parts[i]+'/pcb/'+parts[i]+'.svg', fs.readFileSync('./fritzing/fritzing/parts/svg/core/'+pcbFilepath))
+  } catch(e) {
+    console.log('pcb: '+e);
+  }
+
+  var schematicFilepath = fzpJson.views.schematic.image;
+  try  {
+    fs.writeFileSync('./parts/'+parts[i]+'/schematic/'+parts[i]+'.svg', fs.readFileSync('./fritzing/fritzing/parts/svg/core/'+schematicFilepath))
+  } catch(e) {
+    console.log('schematic: '+e);
+  }
+
+  var iconFilepath = fzpJson.views.icon.image;
+  try  {
+    fs.writeFileSync('./parts/'+parts[i]+'/icon/'+parts[i]+'.svg', fs.readFileSync('./fritzing/fritzing/parts/svg/core/'+iconFilepath))
+  } catch(e) {
+    console.log('icon: '+e);
+  }
+
+};
