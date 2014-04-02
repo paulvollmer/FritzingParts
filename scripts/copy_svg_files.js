04423aaf5651739da7fa0628106f7e422ed0bf1e
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2014-04-02 13:43:05 +0200

    Added sag type to end of sag filename, run make copy_svg_files

--- scripts/copy_svg_files.js
+++ scripts/copy_svg_files.js
@@ -9,28 +9,28 @@ for (var i=0; i<parts.length; i++) {
 
   var breadboardFilepath = fzpJson.views.breadboard.image;
   try  {
-    fs.writeFileSync('./parts/'+parts[i]+'/breadboard/'+parts[i]+'.svg', fs.readFileSync('./fritzing/fritzing/parts/svg/core/'+breadboardFilepath))
+    fs.writeFileSync('./parts/'+parts[i]+'/breadboard/'+parts[i]+'_breadboard.svg', fs.readFileSync('./fritzing/fritzing/parts/svg/core/'+breadboardFilepath))
   } catch(e) {
     console.log('breadboard: '+e);
   }
 
   var pcbFilepath = fzpJson.views.pcb.image;
   try  {
-    fs.writeFileSync('./parts/'+parts[i]+'/pcb/'+parts[i]+'.svg', fs.readFileSync('./fritzing/fritzing/parts/svg/core/'+pcbFilepath))
+    fs.writeFileSync('./parts/'+parts[i]+'/pcb/'+parts[i]+'_pcb.svg', fs.readFileSync('./fritzing/fritzing/parts/svg/core/'+pcbFilepath))
   } catch(e) {
     console.log('pcb: '+e);
   }
 
   var schematicFilepath = fzpJson.views.schematic.image;
   try  {
-    fs.writeFileSync('./parts/'+parts[i]+'/schematic/'+parts[i]+'.svg', fs.readFileSync('./fritzing/fritzing/parts/svg/core/'+schematicFilepath))
+    fs.writeFileSync('./parts/'+parts[i]+'/schematic/'+parts[i]+'_schematic.svg', fs.readFileSync('./fritzing/fritzing/parts/svg/core/'+schematicFilepath))
   } catch(e) {
     console.log('schematic: '+e);
   }
 
   var iconFilepath = fzpJson.views.icon.image;
   try  {
-    fs.writeFileSync('./parts/'+parts[i]+'/icon/'+parts[i]+'.svg', fs.readFileSync('./fritzing/fritzing/parts/svg/core/'+iconFilepath))
+    fs.writeFileSync('./parts/'+parts[i]+'/icon/'+parts[i]+'_icon.svg', fs.readFileSync('./fritzing/fritzing/parts/svg/core/'+iconFilepath))
   } catch(e) {
     console.log('icon: '+e);
   }
