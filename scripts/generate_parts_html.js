175260a01162737950d95cc4dc427ff28b949c1a
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2014-04-03 13:50:08 +0200

    Added line break to generate parts html script

--- scripts/generate_parts_html.js
+++ scripts/generate_parts_html.js
@@ -4,89 +4,89 @@ var fs = require('fs');
 var util = require('./utils');
 var parts = require('../parts.json');
 
-for (var i=0; i<parts.length; i++) {
+for (var i=0; i<0/*parts.length*/; i++) {
   console.log('generate parts html file. '+parts[i]);
   var html = util.defaultJekyllHead(parts[i]);
 
-  html += '<div id="part">';
+  html += '<div id="part">\n';
 
   // The part svg's
-  html += '<table id="part">';
-  html += '<tr>';
-  html += '<td>'+partLink('icon', parts[i], 'Icon')+'</td>';
-  html += '<td>'+partLink('icon', parts[i], 'Breadboard')+'</td>';
-  html += '<td>'+partLink('icon', parts[i], 'PCB')+'</td>';
-  html += '<td>'+partLink('icon', parts[i], 'Schematic')+'</td>';
-  html += '</tr>';
-  html += '<tr>';
-  html += '<td>'+partSvg('icon', parts[i])+'</td>';
-  html += '<td>'+partSvg('breadboard', parts[i])+'</td>';
-  html += '<td>'+partSvg('pcb', parts[i])+'</td>';
-  html += '<td>'+partSvg('schematic', parts[i])+'</td>';
-  html += '</tr>';
-  html += '</table>';
+  html += '<table id="part">\n';
+  html += '<tr>\n';
+  html += '<td>'+partLink('icon', parts[i], 'Icon')+'</td>\n';
+  html += '<td>'+partLink('icon', parts[i], 'Breadboard')+'</td>\n';
+  html += '<td>'+partLink('icon', parts[i], 'PCB')+'</td>\n';
+  html += '<td>'+partLink('icon', parts[i], 'Schematic')+'</td>\n';
+  html += '</tr>\n';
+  html += '<tr>\n';
+  html += '<td>'+partSvg('icon', parts[i])+'</td>\n';
+  html += '<td>'+partSvg('breadboard', parts[i])+'</td>\n';
+  html += '<td>'+partSvg('pcb', parts[i])+'</td>\n';
+  html += '<td>'+partSvg('schematic', parts[i])+'</td>\n';
+  html += '</tr>\n';
+  html += '</table>\n';
 
   // Data from the converted .fzp json file
   var fJson = require('../parts/'+parts[i]+'/index.json');
-  html += '<h3>Information:</h3>';
-  html += '<table id="part_info">';
+  html += '<h3>Information:</h3>\n';
+  html += '<table id="part_info">\n';
   
-  html += '<tr>';
-  html += '<td>fritzing version:</td>';
-  html += '<td>'+fJson.fritzingVersion+'</td>';
-  html += '</tr>';
+  html += '<tr>\n';
+  html += '<td>fritzing version:</td>\n';
+  html += '<td>'+fJson.fritzingVersion+'</td>\n';
+  html += '</tr>\n';
 
-  html += '<tr>';
-  html += '<td>module id:</td>';
-  html += '<td>'+fJson.moduleId+'</td>';
-  html += '</tr>';
+  html += '<tr>\n';
+  html += '<td>module id:</td>\n';
+  html += '<td>'+fJson.moduleId+'</td>\n';
+  html += '</tr>\n';
   
-  html += '<tr>';
-  html += '<td>version:</td>';
-  html += '<td>'+fJson.version+'</td>';
-  html += '</tr>';
+  html += '<tr>\n';
+  html += '<td>version:</td>\n';
+  html += '<td>'+fJson.version+'</td>\n';
+  html += '</tr>\n';
 
-  html += '<tr>';
-  html += '<td>author:</td>';
-  html += '<td>'+fJson.author+'</td>';
-  html += '</tr>';
+  html += '<tr>\n';
+  html += '<td>author:</td>\n';
+  html += '<td>'+fJson.author+'</td>\n';
+  html += '</tr>\n';
 
-  html += '<tr>';
-  html += '<td>title:</td>';
-  html += '<td>'+fJson.title+'</td>';
-  html += '</tr>';
+  html += '<tr>\n';
+  html += '<td>title:</td>\n';
+  html += '<td>'+fJson.title+'</td>\n';
+  html += '</tr>\n';
 
-  html += '<tr>';
-  html += '<td>label:</td>';
-  html += '<td>'+fJson.label+'</td>';
-  html += '</tr>';
+  html += '<tr>\n';
+  html += '<td>label:</td>\n';
+  html += '<td>'+fJson.label+'</td>\n';
+  html += '</tr>\n';
 
-  html += '<tr>';
-  html += '<td>url:</td>';
-  html += '<td>'+fJson.url+'</td>';
-  html += '</tr>';
+  html += '<tr>\n';
+  html += '<td>url:</td>\n';
+  html += '<td>'+fJson.url+'</td>\n';
+  html += '</tr>\n';
 
-  html += '<tr>';
-  html += '<td>date:</td>';
-  html += '<td>'+fJson.date+'</td>';
-  html += '</tr>';
+  html += '<tr>\n';
+  html += '<td>date:</td>\n';
+  html += '<td>'+fJson.date+'</td>\n';
+  html += '</tr>\n';
 
-  html += '<tr>';
-  html += '<td>tags:</td>';
-  html += '<td>'+fJson.tags+'</td>';
-  html += '</tr>';
+  html += '<tr>\n';
+  html += '<td>tags:</td>\n';
+  html += '<td>'+fJson.tags+'</td>\n';
+  html += '</tr>\n';
 
-  html += '<tr>';
-  html += '<td>description:</td>';
-  html += '<td>'+fJson.description+'</td>';
-  html += '</tr>';
+  html += '<tr>\n';
+  html += '<td>description:</td>\n';
+  html += '<td>'+fJson.description+'</td>\n';
+  html += '</tr>\n';
   
-  html += '</table>';
+  html += '</table>\n';
 
-  html += '<h3>FZP and JSON Files:</h3>';
-  html += '<p><a href="./'+parts[i]+'/index.fzp">fzp file</a></p>';
-  html += '<p><a href="./'+parts[i]+'/index.json">json file</a></p>';
-  html += '<div/>';
+  html += '<h3>FZP and JSON Files:</h3>\n';
+  html += '<p><a href="./'+parts[i]+'/index.fzp">fzp file</a></p>\n';
+  html += '<p><a href="./'+parts[i]+'/index.json">json file</a></p>\n';
+  html += '<div/>\n';
 
   fs.writeFileSync('./parts/'+parts[i]+'.html', html);
 }
