17465d4662e752cb21d9a94839edb7ba0a0cfc02
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2014-04-04 00:47:26 +0200

    Cleanup, add comments to "write_parts_json" script

--- scripts/write_parts_json.js
+++ scripts/write_parts_json.js
@@ -1,21 +1,27 @@
-// scripts/generate_parts_json.js
+// scripts/write_parts_json.js
+//
+// List all files from the "fritzing/pdb/core" directory
+// (exclude the placeholder.txt file)
 
 var fs = require('fs');
 
+
+// Read the directory
 fs.readdir('./fritzing/fritzing/pdb/core', function(err, parts) {
+  
   // Find and remove the "placeholder.txt" item from this array.
   var i = parts.indexOf('placeholder.txt');
   if(i != -1) {
     parts.splice(i, 1);
   }
-
+  
   // remove the .fzp suffix
   for (var i=0; i<parts.length; i++) {
     var tmp = parts[i].split('.fzp');
     parts[i] = tmp[0];
-  };
+  }
   
-  // stringity and print to console.
+  // stringify and save to file.
   var partsJson = JSON.stringify(parts, null, 2);
-  console.log(partsJson);
+  fs.writeFileSync('./parts.json', partsJson);
 });
