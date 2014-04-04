d12aa2a125e8d68f2e8e062092f001a0f941ab7b
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2014-04-04 10:04:08 +0200

    Fixed small link 'double slash' bug

--- scripts/generate_parts_overview_html.js
+++ scripts/generate_parts_overview_html.js
@@ -11,11 +11,11 @@ html += '<table>\n';
 // the parts
 for (var i=0; i<parts.length; i++) {
   html += '<tr>\n';
-  html += '<td><a href="{{ site.baseurl }}/parts/'+parts[i]+'.html">'+parts[i]+'</a></td>\n';
-  html += '<td><a href="{{ site.baseurl }}/parts/'+parts[i]+'/icon/'+parts[i]+'_icon.svg">icon</a></td>\n';
-  html += '<td><a href="{{ site.baseurl }}/parts/'+parts[i]+'/breadboard/'+parts[i]+'_breadboard.svg">breadboard</a></td>\n';
-  html += '<td><a href="{{ site.baseurl }}/parts/'+parts[i]+'/pcb/'+parts[i]+'_pcb.svg">pcb</a></td>\n';
-  html += '<td><a href="{{ site.baseurl }}/parts/'+parts[i]+'/schematic/'+parts[i]+'_schematic.svg">schematic</a></td>\n';
+  html += '<td><a href="{{ site.baseurl }}parts/'+parts[i]+'.html">'+parts[i]+'</a></td>\n';
+  html += '<td><a href="{{ site.baseurl }}parts/'+parts[i]+'/icon/'+parts[i]+'_icon.svg">icon</a></td>\n';
+  html += '<td><a href="{{ site.baseurl }}parts/'+parts[i]+'/breadboard/'+parts[i]+'_breadboard.svg">breadboard</a></td>\n';
+  html += '<td><a href="{{ site.baseurl }}parts/'+parts[i]+'/pcb/'+parts[i]+'_pcb.svg">pcb</a></td>\n';
+  html += '<td><a href="{{ site.baseurl }}parts/'+parts[i]+'/schematic/'+parts[i]+'_schematic.svg">schematic</a></td>\n';
   html += '</tr>\n';
 };
 html += '<table>\n';
