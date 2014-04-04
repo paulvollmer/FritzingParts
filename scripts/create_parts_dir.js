bd61ce1f7557860d1da15e1e5fd8b3d93b6ef189
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2014-04-04 11:02:54 +0200

    Added more description

--- scripts/create_parts_dir.js
+++ scripts/create_parts_dir.js
@@ -1,12 +1,12 @@
-// scripts/generate_parts_dir.js
+// scripts/create_parts_dir.js
 
 var fs = require('fs');
 var parts = require('../parts.json');
 
-// create a parts directory
+console.log('create a parts directory');
 fs.mkdirSync('./parts', 0777);
 
-// generate a folder for each part
+console.log('generate a folder for each part and into this directory, create an icon, breadboard, pcb and schematic dir.');
 for (var i=0; i<parts.length; i++) {
   //console.log(parts[i]);
   fs.mkdirSync('./parts/'+parts[i], 0777);
@@ -15,3 +15,5 @@ for (var i=0; i<parts.length; i++) {
   fs.mkdirSync('./parts/'+parts[i]+'/schematic', 0777);
   fs.mkdirSync('./parts/'+parts[i]+'/pcb', 0777);
 };
+
+console.log('Parts directories successfully written!');
