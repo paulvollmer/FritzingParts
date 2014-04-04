0065de3371f1a2253e7540cfebe3137dff2a6375
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2014-04-04 10:29:53 +0200

    Changed comments to console log

--- scripts/write_parts_json.js
+++ scripts/write_parts_json.js
@@ -5,23 +5,24 @@
 
 var fs = require('fs');
 
-
-// Read the directory
+console.log('Read the directory');
 fs.readdir('./fritzing/fritzing/pdb/core', function(err, parts) {
   
-  // Find and remove the "placeholder.txt" item from this array.
+  console.log('Find and remove the "placeholder.txt" item from this array.');
   var i = parts.indexOf('placeholder.txt');
   if(i != -1) {
     parts.splice(i, 1);
   }
   
-  // remove the .fzp suffix
+  console.log('Remove the ".fzp" suffix');
   for (var i=0; i<parts.length; i++) {
     var tmp = parts[i].split('.fzp');
     parts[i] = tmp[0];
   }
   
-  // stringify and save to file.
+  console.log('Stringify and save to file.');
   var partsJson = JSON.stringify(parts, null, 2);
   fs.writeFileSync('./parts.json', partsJson);
+
+  console.log('Ready, "parts.json" successfully written!');
 });
