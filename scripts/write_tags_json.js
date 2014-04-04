b61a94f0318f3cd609514fc23dc28776fce02ecf
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2014-04-04 14:01:07 +0200

    cleanup write_tags_json script

--- scripts/write_tags_json.js
+++ scripts/write_tags_json.js
@@ -1,43 +1,26 @@
 // scripts/write_tags_json.js
 
-// at the moment this is totally bullshit implementation!!!
-// 
-// at every loop we read the parts index.json and check if the tag exists....
-// but it works :)
-
-
-// save the parts index.json object to an array
-// this is temporary used.
-var parts = require('../parts.json');
-var partsJson = [];
-
-for (var i = 0; i < parts.length; i++) {
-  //console.log('part: '+parts[i]);
-  var partData = require('../parts/'+parts[i]+'/index.json');
-  partsJson.push({name: parts[i], data: partData});
-};
-
-
 
 var fs = require('fs');
 var tags = require('../tags.json');
+var partsData = require('./_temp_parts_data.json');
 
 for (var i=0; i<tags.length; i++) {
   var data = [];
 
   // search at all part dataset if the current tag exists.
-  for (var p=0; p<partsJson.length; p++) {
+  for (var p=0; p<partsData.length; p++) {
     // console.log('------------------');
     // console.log('tag:  '+tags[i]);
-    // console.log('name: '+partsJson[p].name);
+    // console.log('name: '+partsData[p].name);
 
-    if (partsJson[p].data.tags != null) {
-      //console.log('tags: '+partsJson[p].data.tags);
+    if (partsData[p].data.tags != null) {
+      //console.log('tags: '+partsData[p].data.tags);
 
-      if (partsJson[p].data.tags.indexOf(tags[i]) !== -1) {
-        console.log(tags[i], 'treffer! ', partsJson[p].name, ' -- tags:', partsJson[p].data.tags);
+      if (partsData[p].data.tags.indexOf(tags[i]) !== -1) {
+        console.log(tags[i], 'treffer! ', partsData[p].name, ' -- tags:', partsData[p].data.tags);
 
-        data.push(partsJson[p].name);
+        data.push(partsData[p].name);
       };
     };
   };
@@ -63,6 +46,5 @@ for (var i=0; i<tags.length; i++) {
   //console.log(filename);
 
 
-  
   fs.writeFileSync('./tags/'+filename+'.json', JSON.stringify(data, null, 2));
 };
